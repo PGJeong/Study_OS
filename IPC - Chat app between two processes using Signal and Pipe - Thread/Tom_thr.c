e6c4169ec71e3797dfdfbdd82d9961b080ba1e99
PGJeong <pilgyo718@gmail.com>
2022-11-08 22:07:08 +0900

    modify (IPC - Chat app2)

--- IPC - Chat app between two processes using Signal and Pipe - Thread/Tom_thr.c
+++ IPC - Chat app between two processes using Signal and Pipe - Thread/Tom_thr.c
@@ -16,9 +16,12 @@ pthread_t receiverTID, senderTID; // Thread ID(TID)를 저장하는 변수
 pthread_attr_t receiverAttr, senderAttr; // Thread 속성(attribute)을 저장하는 객체
 
 void sigint(int sig) { // SIGINT 핸들러
-
-	repeat = 0;
 	
+	repeat = 0;
+	close(fdw); // FIFO의 한 쪽 끝단(쓰기모드) 닫기
+	close(fdr); // FIFO의 한 쪽 끝단(읽기모드) 닫기
+	
+	// FIFO가 열려있는 경우, Receiver의 read()와 Sender의 write()가 정상 대기하고 있기 때문에 스레드가 죽지 않으므로 SIGINT 핸들러에서 닫아준다
 	// pthread_equal(pthread_t t1, pthread_t t2) : 두 스레드의 ID가 동일하면 0이 아닌 값을 반환 (리턴타입 : int)
 }
 
@@ -38,7 +41,7 @@ void* receiver() {
 	}
 
 	printf("\nReceiver Thread End (TID : %ld)", pthread_self());
-	pthread_exit((void*)21);
+	pthread_exit((void*)1);
 	// pthread_exit (void *return_value) : 해당 스레드만 종료 (다른 스레드에 영향 X)
 }
 
@@ -55,7 +58,7 @@ void* sender() {
 	}
 
 	printf("\nSender Thread End (TID : %ld)", pthread_self());
-	pthread_exit((void*)22);
+	pthread_exit((void*)2);
 	// pthread_exit (void *return_value) : 해당 스레드만 종료 (다른 스레드에 영향 X)
 }
 
@@ -78,7 +81,7 @@ int main()
 	fdr = open(Jerry2Tom, O_RDONLY); // 읽기모드(O_RDONLY)로 열기. fdr 변수는 File descriptor
 
 	/* Thread 생성 */
-	
+
 	pthread_attr_init(&receiverAttr); // Thread 속성 객체를 기본 속성 값들로 초기화
 	pthread_attr_init(&senderAttr);
 
@@ -94,10 +97,7 @@ int main()
 	pthread_join(senderTID, (void*)&sender_return_val); // Sender Thread가 종료할 때까지 대기 (return_val : 스레드 종료시 반환값 저장할 곳)
 	printf("\nSender Thread  (TID : %ld) Return Value : %d", senderTID, sender_return_val);
 
-	close(fdw); // FIFO의 한 쪽 끝단(쓰기모드) 닫기
-	close(fdr); // FIFO의 한 쪽 끝단(읽기모드) 닫기
-
-	printf("\nMain Thread End (TID : %lu)", pthread_self());
+	printf("\nMain Thread End (TID : %lu)\n", pthread_self());
 
 	return 0;
 }
